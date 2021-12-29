19b92fc69dcd4b29c5a65d8dd0d6bcfa2572b99d
Michael McLean <g4m3rm1k3@hotmail.com>
2021-12-29 04:11:14 -0500

    added storage

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,27 +1,50 @@
-const notes = [
-	{
-		title: "My next trip",
-		body: "I would like to go to Spain",
-	},
-	{
-		title: "Habbits to work on",
-		body: "Exercise, Eating a bit better",
-	},
-	{
-		title: "Office modification",
-		body: "Get a new seat",
-	},
-];
-document.querySelector("#create-note").addEventListener("click", function (e) {
-	console.log("Did this work");
-});
+let notes = [];
 
-document.querySelector("#remove-all").addEventListener("click", function (e) {
-	document.querySelectorAll(".note").forEach(function (note) {
-		note.remove();
+const filters = {
+	searchText: "",
+};
+
+// Check for existing saved data
+const notesJSON = localStorage.getItem("notes");
+
+if (notesJSON !== null) {
+	notes = JSON.parse(notesJSON);
+}
+
+const renderNotes = function (notes, filters) {
+	const filteredNotes = notes.filter(function (note) {
+		return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
 	});
+
+	document.querySelector("#notes").innerHTML = "";
+
+	filteredNotes.forEach(function (note) {
+		const newNote = document.createElement("p");
+		if (note.title.length > 0) {
+			newNote.textContent = note.body;
+		} else {
+			newNote.textContent = "Unnamed note";
+		}
+		document.querySelector("#notes").appendChild(newNote);
+	});
+};
+
+renderNotes(notes, filters);
+
+document.querySelector("#create-note").addEventListener("click", function (e) {
+	notes.push({
+		title: "",
+		body: "",
+	});
+	localStorage.setItem("notes", JSON.stringify(notes));
+	renderNotes(notes, filters);
 });
 
 document.querySelector("#search-text").addEventListener("input", function (e) {
+	filters.searchText = e.target.value;
+	renderNotes(notes, filters);
+});
+
+document.querySelector("#filter-by").addEventListener("change", function (e) {
 	console.log(e.target.value);
 });
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,31 +1,60 @@
-const todos = [
-	{ text: "finish javascript course", completed: true },
-	{ text: "finish node course", completed: false },
-	{ text: "take express course", completed: false },
-	{ text: "take react course", completed: true },
-	{ text: "start django react course", completed: false },
-];
+let todos = [];
+
+const todosJSON = localStorage.getItem("todos");
+
+if (todosJSON !== null) {
+	todos = JSON.parse(todosJSON);
+}
 
 // Summary message You have 2 todos left (p element)
 // Add a p for each todo above (use text value)
 
-const todosLeft = todos.filter(function (todo) {
-	return !todo.completed;
-}).length;
+const filters = {
+	searchText: "",
+};
 
-const p = document.createElement("h2");
-p.textContent = `You have ${todosLeft} todos left`;
-document.body.appendChild(p);
-todos.forEach(function (todo) {
-	const p = document.createElement("p");
-	p.textContent = todo.text;
-	document.body.appendChild(p);
+const filteredTodos = function (todos, f) {
+	const todoPlace = document.querySelector("#todoPLace");
+	todoPlace.innerHTML = "";
+
+	const filtered = todos.filter(function (todo) {
+		const searchTextMatch = todo.text.toLowerCase().includes(f.toLowerCase());
+		const hideCompletedMatch = !filters.hide || !todo.completed;
+		return searchTextMatch && hideCompletedMatch;
+	});
+
+	const todosLeft = filtered.filter(function (todo) {
+		return !todo.completed;
+	}).length;
+
+	const p = document.createElement("h2");
+	p.textContent = `You have ${todosLeft} todos left`;
+	todoPlace.appendChild(p);
+
+	filtered.forEach(function (todo) {
+		const p = document.createElement("p");
+		p.textContent = todo.text;
+		todoPlace.appendChild(p);
+	});
+};
+
+filteredTodos(todos, "");
+
+document.querySelector("#search-text").addEventListener("input", function (e) {
+	filteredTodos(todos, e.target.value);
 });
 
-const button = document.createElement("button");
-button.textContent = "Add Todos";
-button.setAttribute("id", "add-todo");
-document.body.appendChild(button);
-button.addEventListener("click", function (e) {
-	e.target.textContent = "Added a new todo";
+document.querySelector("#add-todo").addEventListener("submit", function (e) {
+	e.preventDefault();
+	todos.push({ text: e.target.elements.todo.value, completed: false });
+	localStorage.setItem("todos", JSON.stringify(todos));
+	e.target.elements.todo.value = "";
+	filteredTodos(todos, "");
+});
+
+document.querySelector("#toggle").addEventListener("change", function (e) {
+	//
+
+	filters.hide = e.target.checked;
+	filteredTodos(todos, "");
 });
